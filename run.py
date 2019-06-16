812cb190d7fbdd2a61e0edbb3794ca70f6ee90fd
swati <swati.sneha095@gmail.com>
2019-06-16 08:19:26 +0530

    Comments added and Database Integration

--- run.py
+++ run.py
@@ -1,32 +1,62 @@
-# from telegram.ext import Updater, CommandHandler
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line to stop the
+bot.
+"""
 
 
-# def hello(bot, update):
-#
-#     update.message.reply_text(
-#         'Hello {}'.format(update.message.from_user.first_name))
-
-
-token = "890443159:AAEVeJJY-0puigHnIAO6_IxXmO9VIfwJcc0"
-typing_time = 3 # seconds
-
+from configparser import ConfigParser
 import time
 import logging
-from telegram.ext import (Filters, Updater, CommandHandler, InlineQueryHandler, ConversationHandler, RegexHandler, MessageHandler )
+from telegram.ext import Filters, Updater, CommandHandler, InlineQueryHandler, ConversationHandler, RegexHandler, MessageHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction
 from intents import intents
+import util_funcs as uf
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
 CHOOSING_START, SUBSCRIBE, COMPANY, TYPING_REPLY  = range(4)
 
+'''Reading config details'''
+config = ConfigParser()
+config.readfp(open('intents.config'))
+token = config.get('settings', 'token')
+typing_time = config.getint('settings', 'typing_time')
+db_url=config.get('settings', 'DB_URL')
+
+
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+meta = MetaData()
+engine=create_engine(db_url, echo=True)
+
+'''Initialising table'''
+users = Table(
+   'users', meta, 
+   Column('email_id', String, primary_key = True), 
+   Column('company_name', String), 
+)
+
+
+
 def start(update, context):
+
+    '''starts the conversation with /start -- trigger point of chat'''
+
     welcome = intents['welcome']
-    markup = [[_ans['answer'] for _ans in welcome['answers']]]
+    markup = [[_ans['answer'] for _ans in welcome['answers']]]  
     update.message.reply_text(
         welcome['bot'][0],
         reply_markup=ReplyKeyboardMarkup(markup, one_time_keyboard=True))
@@ -34,13 +64,17 @@ def start(update, context):
     return CHOOSING_START
 
 def know_more(update, context):
+
+    '''fetches description from intents and displays and gives further options to user'''
+    
     intent_description = intents['insent_description']
     msgs = intent_description['bot']
     for _msg in msgs:
-        context.bot.send_chat_action(chat_id=update.message.chat_id ,action = ChatAction.TYPING)
+        context.bot.send_chat_action(chat_id=update.message.chat_id ,action = ChatAction.TYPING) #typing_action shown
         time.sleep(typing_time)
         update.message.reply_text(_msg)
-    markup = [[_ans['answer'] for _ans in intent_description['answers']]]
+
+    markup = [[_ans['answer'] for _ans in intent_description['answers']]]    
 
     update.message.reply_text(
         'Please choose',
@@ -49,6 +83,9 @@ def know_more(update, context):
     return SUBSCRIBE
 
 def talk_to_team(update, context):
+
+    '''fetches the messges for connect_insent_team and displays the options accordingly'''
+
     subscribe = intents['connect_insent_team']
     msgs = subscribe['bot']
     for _msg in msgs:
@@ -67,7 +104,7 @@ def talk_to_team(update, context):
 
 
 def email(update, context):
-    context.user_data['choice'] = 'email'
+    context.user_data['choice'] = 'email' 
     subscribe = intents['subscribe_insent']
     msgs = subscribe['bot']
     update.message.reply_text(msgs[0])
@@ -80,21 +117,15 @@ def company(update, context):
     update.message.reply_text(msgs[0])
     return TYPING_REPLY
 
-def facts_to_str(user_data):
-    facts = list()
-    for key, value in user_data.items():
-        facts.append('{} - {}'.format(key, value))
-
-    return "\n".join(facts).join(['\n', '\n'])
 
 def received_information(update, context):
+    '''saves the data -company and email to the context'''
     user_data = context.user_data
     text = update.message.text
     category = user_data.get('choice', '')
     user_data[category] = text
     if category:
         del user_data['choice']
-
     if category == 'email':
         company(update, context)
     else:
@@ -114,7 +145,13 @@ def done(update, context):
     try:
         update.message.reply_text("Thanks")
         user_data = context.user_data
+        '''saving the details to database before deleting'''
+        ins=users.insert()
+        ins = users.insert().values(email_id=context.user_data['email'], company_name=context.user_data['company'])
+        conn = engine.connect()
+        result = conn.execute(ins)
         user_data.clear()
+
         return ConversationHandler.END
     except:
         return ConversationHandler.END
@@ -122,10 +159,9 @@ def done(update, context):
 
 def main():
     try:
+        #Create the Updater and pass it your bot's token.s
         updater = Updater(token, use_context=True)
-
         dp = updater.dispatcher
-
         conv_handler = ConversationHandler(
             entry_points=[CommandHandler('start', start)],
 
@@ -164,12 +200,19 @@ def main():
             allow_reentry= True
         )
 
+        #handler to hold a conversation with a single user
         dp.add_handler(conv_handler)
 
+        # log all errors
         dp.add_error_handler(error)
 
+        # Start the bot
         updater.start_polling()
 
+
+
+        # Run the bot until Ctrl-C is pressed 
+        # start_polling() is non-blocking
         updater.idle()
     except:
         return ConversationHandler.END
